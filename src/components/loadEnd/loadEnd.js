6dd6b09bf64a6fba0ce832d75c8b15f31051872b
Yahoo-back <2928501731@qq.com>
2018-12-28 11:45:44 +0800

    添加标签查询收缩框

--- src/components/loadEnd/loadEnd.js
+++ src/components/loadEnd/loadEnd.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 
 class LoadEndCom extends Component {
-	render() {
-		const style = {
-			color: '#999',
-			textAlign: 'center',
-			padding: 50,
-			fontSize: 16,
-		};
-		return (
-			<div style={style}> --------- 我也是有底线的啦 --------- </div>
-		);
-	}
+  render() {
+    const style = {
+      color: '#999',
+      textAlign: 'center',
+      padding: 50,
+      fontSize: 16
+    };
+    return <div style={style}> </div>;
+  }
 }
 
 export default LoadEndCom;
